16f0a1b915d12158466481b32222e7e377587d23
leonardo <leonardo@mail.equipo5.localhost>
2023-04-18 12:53:02 -0600

    inicio de lista

---
+++ src/App.jsx
@@ -0,0 +1,44 @@
+function App() {
+
+  return (
+
+ <div className="container text-center">
+  <div className="row">
+    <div className="col">
+      <h1>Shopping List</h1>
+      <br />
+    </div>
+  </div>
+  <div className="row">
+    <div className="col">
+      C
+    </div>
+    <div className="col-8">
+      Primer Elemento de mi Lista 
+    </div>
+    <div className="col-3">
+      Botones
+    </div>
+  </div>
+  <br />
+  <div className="row">
+    <div className="col">
+      C
+    </div>
+    <div className="col-8">
+      Segundo Elemento de mi Lista
+    </div>
+    <div className="col-3">
+      Botones
+    </div>
+  </div>
+  <br />
+ </div>
+ 
+   
+
+  
+
+  )
+  }
+export default App
